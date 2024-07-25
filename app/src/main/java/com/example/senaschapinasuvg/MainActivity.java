5f8801b80d9fdee64c3aa7cd6db466837752bf9b
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2024-07-24 23:57:39 -0600

    strucutura 2

--- app/src/main/java/com/example/senaschapinasuvg/MainActivity.java
+++ app/src/main/java/com/example/senaschapinasuvg/MainActivity.java
@@ -1,4 +1,28 @@
 package com.example.senaschapinasuvg;
 
-public class MainActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configurar NavController
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = ((NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment))
+                .getNavController();
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
 }
