9c07cfcce83e268344808bb183bcab8239886ca1
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2024-08-06 14:38:52 -0600

    Record button

--- components/src/main/java/com/example/components/buttons/RecordButton.java
+++ components/src/main/java/com/example/components/buttons/RecordButton.java
@@ -1,27 +1,40 @@
 package com.example.components.buttons;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.TransitionDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.CountDownTimer;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
 import com.example.components.R;
+import com.example.components.buttons.DebounceClickListener;
+import com.example.components.buttons.IButtons;
 
-public class RecordButton extends ConstraintLayout implements  IButtons {
+public class RecordButton extends ConstraintLayout implements IButtons {
 
-    //Atributos del componente ---------------------------------------------------------------------
-    private Button button;
-    private OnRecordListener onRecordListener; // Listener para los eventos de grabación
+    private Button pre_record;
+    private ConstraintLayout recording;
+    private ImageView front, back;
+    private TextView timer;
+    private ProgressBar progressBar;
+    private OnRecordListener onRecordListener;
+    private CountDownTimer countDownTimer;
 
-
-    // Metodos de ContraintLayout ------------------------------------------------------------------
     public RecordButton(Context context) {
         super(context);
         initView();
@@ -41,7 +54,13 @@ public class RecordButton extends ConstraintLayout implements  IButtons {
 
     private void initView() {
         inflate(getContext(), R.layout.record_button, this);
-        button = findViewById(R.id.btn_main);
+        pre_record = findViewById(R.id.btn_pre_record);
+        recording = findViewById(R.id.btn_recording);
+        front = findViewById(R.id.imgFront);
+        back = findViewById(R.id.img_back);
+        timer = findViewById(R.id.timer_text);
+        progressBar = findViewById(R.id.progress_bar);
+
         setTouchListener();
     }
 
@@ -71,30 +90,38 @@ public class RecordButton extends ConstraintLayout implements  IButtons {
         if (background != null) {
             setBackground(background);
         }
-
     }
 
     private void setTouchListener() {
-        final Drawable normalDrawable = ContextCompat.getDrawable(getContext(), R.drawable.record_button);
-        final Drawable recordingDrawable = ContextCompat.getDrawable(getContext(), R.drawable.recording_button);
-        final TransitionDrawable transitionDrawable = new TransitionDrawable(new Drawable[] { normalDrawable, recordingDrawable });
+        recording.setVisibility(GONE);
+        pre_record.setVisibility(VISIBLE);
+        timer.setVisibility(GONE);
+        progressBar.setVisibility(GONE);
 
-        button.setBackground(normalDrawable);  // Set the initial background
-
-        button.setOnTouchListener(new OnTouchListener() {
+        pre_record.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        button.setBackground(transitionDrawable);
-                        transitionDrawable.startTransition(300); // Start the transition over 300 milliseconds
+                        recording.setVisibility(VISIBLE);
+                        pre_record.setVisibility(INVISIBLE);
+                        timer.setVisibility(VISIBLE);
+                        progressBar.setVisibility(VISIBLE);
+                        progressBar.setProgress(0);
+                        startTimer();
+
+                        animateButton(true);
                         if (onRecordListener != null) {
                             onRecordListener.onStartRecording();
                         }
                         return true;
                     case MotionEvent.ACTION_UP:
                     case MotionEvent.ACTION_CANCEL:
-                        transitionDrawable.reverseTransition(300); // Reverse the transition
+                        recording.setVisibility(GONE);
+                        pre_record.setVisibility(VISIBLE);
+                        stopTimer();
+
+                        animateButton(false);
                         if (onRecordListener != null) {
                             onRecordListener.onStopRecording();
                         }
@@ -108,46 +135,96 @@ public class RecordButton extends ConstraintLayout implements  IButtons {
         });
     }
 
+    private void animateButton(boolean startRecording) {
+        ObjectAnimator imgBackGrowX = ObjectAnimator.ofFloat(back, "scaleX", startRecording ? 1.3f : 1f);
+        ObjectAnimator imgBackGrowY = ObjectAnimator.ofFloat(back, "scaleY", startRecording ? 1.3f : 1f);
+        ObjectAnimator imgFrontShrinkX = ObjectAnimator.ofFloat(front, "scaleX", startRecording ? 0.5f : 1f);
+        ObjectAnimator imgFrontShrinkY = ObjectAnimator.ofFloat(front, "scaleY", startRecording ? 0.5f : 1f);
 
-    @Override
-    public boolean performClick() {
-        // Call the superclass to handle the click
-        return super.performClick();
+        imgBackGrowX.setDuration(300);
+        imgBackGrowY.setDuration(300);
+        imgFrontShrinkX.setDuration(300);
+        imgFrontShrinkY.setDuration(300);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(imgBackGrowX, imgBackGrowY, imgFrontShrinkX, imgFrontShrinkY);
+        animatorSet.start();
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(15000, 50) { // Actualiza cada 50 ms
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (15000 - millisUntilFinished) / 1000;
+                timer.setText(String.format("%02d:%02d", seconds / 60, seconds % 60));
+
+                // Anima el progreso del ProgressBar
+                float progress = (15000 - millisUntilFinished) / 15000.0f * 100;
+                animateProgressBar(progress);
+            }
+
+            @Override
+            public void onFinish() {
+                timer.setText("00:00");
+                animateProgressBar(100); // 100% del progreso
+                recording.setVisibility(GONE);
+                pre_record.setVisibility(VISIBLE);
+                timer.setVisibility(GONE);
+                progressBar.setVisibility(GONE);
+                if (onRecordListener != null) {
+                    onRecordListener.onStopRecording();
+                }
+            }
+        }.start();
+    }
+
+    private void animateProgressBar(float toProgress) {
+        ObjectAnimator progressAnimator = ObjectAnimator.ofInt(progressBar, "progress", progressBar.getProgress(), (int) toProgress);
+        progressAnimator.setDuration(100); // Duración más corta para animaciones más suaves
+        progressAnimator.setInterpolator(new LinearInterpolator()); // Interpolador para animación suave
+        progressAnimator.start();
     }
 
 
-    // Metodos de IButtons -------------------------------------------------------------------------
+    private void stopTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        timer.setText("00:00");
+        progressBar.setProgress(0);
+        timer.setVisibility(GONE);
+        progressBar.setVisibility(GONE);
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
     @Override
     public void setText(String value) {
-        button.setText(value);
     }
 
     @Override
     public void setTextSize(int value) {
-        button.setTextSize(value);
     }
 
     @Override
     public void setEnabled(boolean value) {
-        //no aplica
     }
 
     @Override
     public void setTextColor(int color) {
-        button.setTextColor(color);
     }
 
     @Override
     public void setBackground(Drawable value) {
-        button.setBackground(value);
     }
 
-    // Listener ------------------------------------------------------------------------------------
     public void setOnClickListener(OnClickListener listener) {
-        button.setOnClickListener(new DebounceClickListener(listener));
+        pre_record.setOnClickListener(new DebounceClickListener(listener));
     }
 
-    // Interfaz
     public interface OnRecordListener {
         void onStartRecording();
         void onStopRecording();
@@ -156,8 +233,4 @@ public class RecordButton extends ConstraintLayout implements  IButtons {
     public void setOnRecordListener(OnRecordListener listener) {
         this.onRecordListener = listener;
     }
-
-
-
-
 }
