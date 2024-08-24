d31c6a8707e640c723d7d98804605911ad1d3f2e
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2024-08-23 20:10:15 -0600

    video ya no estirado :)

--- screens/src/main/java/com/example/screens/flows/home/ui/HomeFragment.java
+++ screens/src/main/java/com/example/screens/flows/home/ui/HomeFragment.java
@@ -17,6 +17,18 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.FileOutputOptions;
+import androidx.camera.video.PendingRecording;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.video.VideoRecordEvent;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -41,6 +53,7 @@ import com.example.screens.flows.home.vm.HomeViewModel;
 import com.example.screens.flows.profile.ui.ProfileFragment;
 import com.example.screens.flows.video.vm.VideoViewModel;
 import com.example.screens.utils.SharedPreferencesManager;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.io.IOException;
@@ -51,6 +64,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 public class HomeFragment extends BaseFragment implements View.OnClickListener {
 
@@ -63,17 +77,13 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     private HomeViewModel homeViewModel;
     private SharedPreferencesManager sharedPreferencesManager;
 
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSessions;
-    private CaptureRequest.Builder captureRequestBuilder;
-    private Size imageDimension;
-
-    private boolean isRecording = false;
-    private MediaRecorder mediaRecorder;
-    private String cameraId = null;
     private boolean isUsingFrontCamera = false; // Comienza con la cámara trasera
-
     private String filepath = "";
+
+    private VideoCapture videoCapture;
+    private Recording recording;
+
+
 
     // Metodos de ciclo de vida --------------------------------------------------------------------
 
@@ -81,6 +91,14 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+
+        Recorder recorder = new Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.FHD))
+                .build();
+
+        videoCapture = VideoCapture.withOutput(recorder);
+
+
     }
 
     @Override
@@ -124,14 +142,6 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
 
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    }
 
     @Override
     public void onResume() {
@@ -250,275 +260,66 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     // Camera --------------------------------------------------------------------------------------
 
 
-    private Size chooseOptimalSize(Size[] choices, int textureViewWidth, int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
-        // Coleccionar los tamaños soportados mayores al tamaño de la vista previa
-        List<Size> bigEnough = new ArrayList<>();
-        // Coleccionar los tamaños soportados menores al tamaño de la vista previa
-        List<Size> notBigEnough = new ArrayList<>();
-        int w = aspectRatio.getWidth();
-        int h = aspectRatio.getHeight();
-
-        for (Size option : choices) {
-            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
-                    option.getHeight() == option.getWidth() * h / w) {
-                if (option.getWidth() >= textureViewWidth && option.getHeight() >= textureViewHeight) {
-                    bigEnough.add(option);
-                } else {
-                    notBigEnough.add(option);
-                }
-            }
-        }
-
-        // Elegir el más pequeño de los suficientemente grandes. Si no hay ninguno suficientemente grande, elegir el más grande de los no suficientemente grandes.
-        if (bigEnough.size() > 0) {
-            return Collections.min(bigEnough, new CompareSizesByArea());
-        } else if (notBigEnough.size() > 0) {
-            return Collections.max(notBigEnough, new CompareSizesByArea());
-        } else {
-            Log.e("TAG", "Couldn't find any suitable preview size");
-            return choices[0];
-        }
-    }
-
-    static class CompareSizesByArea implements Comparator<Size> {
-        @Override
-        public int compare(Size lhs, Size rhs) {
-            // Cast to ensure the multiplications won't overflow
-            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
-                    (long) rhs.getWidth() * rhs.getHeight());
-        }
-    }
-
-
-    private void flipCamera() {
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-
-        if (cameraCaptureSessions != null) {
-            cameraCaptureSessions.close();
-            cameraCaptureSessions = null;
-        }
-
-        // Determina si usas la cámara frontal o trasera
-        isUsingFrontCamera = !isUsingFrontCamera;
-
-        // Reabre la cámara con el nuevo ID
-        openCamera(isUsingFrontCamera);
-    }
-
-    private void openCamera(boolean useFrontCamera) {
-        CameraManager manager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            for (String id : manager.getCameraIdList()) {
-                CameraCharacteristics characteristics = manager.getCameraCharacteristics(id);
-                Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
-                if (facing != null) {
-                    if ((useFrontCamera && facing == CameraCharacteristics.LENS_FACING_FRONT) ||
-                            (!useFrontCamera && facing == CameraCharacteristics.LENS_FACING_BACK)) {
-                        cameraId = id;
-                        break;
-                    }
-                }
-            }
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            manager.openCamera(cameraId, stateCallback, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void encenderCamara() {
         binding.clEmpty.setVisibility(View.GONE);
         binding.clCamera.setVisibility(View.VISIBLE);
 
-        CameraManager manager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            cameraId = manager.getCameraIdList()[0];
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
-            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-            assert map != null;
-            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
-            // Abrir la cámara
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(binding.camera.getSurfaceProvider());
+
+                CameraSelector cameraSelector = isUsingFrontCamera
+                        ? CameraSelector.DEFAULT_FRONT_CAMERA
+                        : CameraSelector.DEFAULT_BACK_CAMERA;
+
+                // Ensure videoCapture is only initialized once
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture);
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
             }
-            manager.openCamera(cameraId, stateCallback, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+        }, ContextCompat.getMainExecutor(getContext()));
     }
-
-    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(CameraDevice camera) {
-            cameraDevice = camera;
-            createCameraPreview();
-        }
-
-        @Override
-        public void onDisconnected(CameraDevice camera) {
-            cameraDevice.close();
-        }
-
-        @Override
-        public void onError(CameraDevice camera, int error) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    };
-
-    protected void createCameraPreview() {
-        try {
-            SurfaceTexture texture = binding.camera.getSurfaceTexture();
-            if (texture == null) {
-                Log.e("HomeFragment", "SurfaceTexture es nulo, no se puede crear la vista previa de la cámara");
-                return;
-            }
-
-            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
-            Surface surface = new Surface(texture);
-
-            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            captureRequestBuilder.addTarget(surface);
-
-            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    if (null == cameraDevice) {
-                        return;
-                    }
-                    cameraCaptureSessions = cameraCaptureSession;
-                    updatePreview();
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    Toast.makeText(getContext(), "Error al configurar cámara", Toast.LENGTH_SHORT).show();
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private void updatePreview() {
-        if (null == cameraDevice) {
-            return;
-        }
-        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-        try {
-            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     private void startRecording() {
-        if (cameraDevice == null || !binding.camera.isAvailable()) return;
-
-        try {
-            setUpMediaRecorder();
-            SurfaceTexture texture = binding.camera.getSurfaceTexture();
-            assert texture != null;
-            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
-            Surface previewSurface = new Surface(texture);
-            Surface recordSurface = mediaRecorder.getSurface();
-
-            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
-            captureRequestBuilder.addTarget(previewSurface);
-            captureRequestBuilder.addTarget(recordSurface);
-
-            cameraDevice.createCaptureSession(Arrays.asList(previewSurface, recordSurface),
-                    new CameraCaptureSession.StateCallback() {
-                        @Override
-                        public void onConfigured(@NonNull CameraCaptureSession session) {
-                            cameraCaptureSessions = session;
-                            updatePreview();
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    isRecording = true;
-                                    mediaRecorder.start();
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                            Toast.makeText(getContext(), "Error al configurar cámara", Toast.LENGTH_SHORT).show();
-                        }
-                    }, null);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (videoCapture == null) {
+            return;
         }
+
+        File videoFile = new File(getContext().getExternalFilesDir(null), "video.mp4");
+        filepath = videoFile.getAbsolutePath();
+        FileOutputOptions outputOptions = new FileOutputOptions.Builder(videoFile).build();
+
+        PendingRecording pendingRecording = ((Recorder) videoCapture.getOutput())
+                .prepareRecording(getContext(), outputOptions);
+
+        recording = pendingRecording.start(ContextCompat.getMainExecutor(getContext()), videoRecordEvent -> {
+            if (videoRecordEvent instanceof VideoRecordEvent.Finalize) {
+            }
+        });
     }
 
+
     private void stopRecording() {
-        if (isRecording) {
-            mediaRecorder.stop();
-            mediaRecorder.reset();
-            isRecording = false;
+        if (recording != null) {
+            recording.stop();
+            recording = null; // Reset the recording object
         }
 
         videoTraductionService();
-
     }
 
-    private void setUpMediaRecorder() throws IOException {
-        mediaRecorder = new MediaRecorder();
-        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE); // Fuente de video
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4); // Formato de salida
-
-        // Usar createTemporaryVideoFile para generar la ruta del archivo temporal
-        String tempFilePath = createTemporaryVideoFile(getContext());
-        mediaRecorder.setOutputFile(tempFilePath); // Archivo de salida
-
-        mediaRecorder.setVideoEncodingBitRate(10000000); // Tasa de bits de video
-        mediaRecorder.setVideoFrameRate(30); // Tasa de cuadros de video
-        mediaRecorder.setVideoSize(imageDimension.getWidth(), imageDimension.getHeight()); // Tamaño de video
-        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264); // Codificador de video
-
-        // Configurar la orientación del video
-        CameraManager manager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
-            int sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
-            int deviceOrientation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
-            int rotation = (ORIENTATIONS.get(deviceOrientation) + sensorOrientation + 270) % 360;
-            mediaRecorder.setOrientationHint(rotation);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-        mediaRecorder.prepare(); // Preparar el MediaRecorder para la grabación
-    }
-
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    private void flipCamera() {
+        isUsingFrontCamera = !isUsingFrontCamera;
+        encenderCamara(); // Reinitialize the camera with the new selector
     }
 
 
-
-    private String createTemporaryVideoFile(Context context) throws IOException {
-        // Crea un archivo temporal en el directorio de caché
-        File tempFile = File.createTempFile(
-                "video_", // prefijo
-                ".mp4",   // sufijo
-                context.getCacheDir() // directorio
-        );
-        this.filepath = tempFile.getAbsolutePath();
-        return tempFile.getAbsolutePath();
-    }
 
 
     // Servicios -----------------------------------------------------------------------------------
