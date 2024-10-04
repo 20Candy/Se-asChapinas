4f44e76d59a570d3fccd7fab05786be865d243ea
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2024-10-03 22:15:33 -0600

    actualizacion de link backend

--- services/src/main/java/com/senaschapinas/base/serviceRetrofit.java
+++ services/src/main/java/com/senaschapinas/base/serviceRetrofit.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class serviceRetrofit {
 
-    private static final String BASE_URL = "https://b93c-190-14-11-2.ngrok-free.app";
+    private static final String BASE_URL = "https://01e1-190-56-194-12.ngrok-free.app";
     private static Retrofit retrofit = null;
 
     public static serviceAPI create() {
